938e83622640e6a554447a274d1513cab9ef7341
victoriactemple <victoriactemple@gmail.com>
2017-10-23 19:21:52 -0400

    3 Integer Skills

--- ninja_skills.rb
+++ ninja_skills.rb
@@ -1,31 +1,49 @@
-# Challenge 1: Booleans 
+# # Challenge 1: Booleans 
 
-# 1. Create a variable a and set it equal to true
-a = true
-# 2. Create a variable b and set it equal to false
-b = false
-# 3.  Write a statement using a and b that evaluates to false
-   puts  a === b 
-#       puts the result to the terminal
-# 4. Write a statement using a and b that evaluates to true
-    a != b 
-    puts a != b
-#       puts the result to the terminal
+# # 1. Create a variable a and set it equal to true
+# a = true
+# # 2. Create a variable b and set it equal to false
+# b = false
+# # 3.  Write a statement using a and b that evaluates to false
+#    puts  a === b 
+# #       puts the result to the terminal
+# # 4. Write a statement using a and b that evaluates to true
+#     a != b 
+#     puts a != b
+# #       puts the result to the terminal
 
 
 
-# ---------------------------------------------------
+# # ---------------------------------------------------
 
-# Challenge 2: Nil
+# # Challenge 2: Nil
 
-# 1 Create a variable of your choosing and set it equal to Nil
-my_nil_variable = nil
-# p the value of the variable to the terminal
-puts my_nil_variable
-# puts the value of the variable to the terminal concatenate the string "nil was here".
-puts "#{my_nil_variable} + nil was here"
-# print the value of the variable to the terminal
-print my_nil_variable
-# puts the class of the value of the variable to the terminal. Use the .class method. Look up this method on Google.
-    puts nil.class
+# # 1 Create a variable of your choosing and set it equal to Nil
+# my_nil_variable = nil
+# # p the value of the variable to the terminal
+# puts my_nil_variable
+# # puts the value of the variable to the terminal concatenate the string "nil was here".
+# puts "#{my_nil_variable} + nil was here"
+# # print the value of the variable to the terminal
+# print my_nil_variable
+# # puts the class of the value of the variable to the terminal. Use the .class method. Look up this method on Google.
+#     puts nil.class
 
+
+
+#  ------------------------------------------------------
+# Challenge 3: Integers
+
+# 1.  Create a variable d and set it equal to a Fixnum
+#       puts the value of d to the terminal
+d = 9
+puts d
+# 2.  Create a variable e and set it equal to a Float
+e = 4.6
+puts e
+#       puts the value of e to the terminal
+# 3.  Set d equal to e on another line
+d = e
+
+#        puts the value of d to the terminal
+puts d
