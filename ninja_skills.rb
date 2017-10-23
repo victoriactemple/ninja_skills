2de6df077df1bc5edd2ddafce7b43d0df2dffb53
victoriactemple <victoriactemple@gmail.com>
2017-10-23 17:13:01 -0400

    1 Booleans Skills

---
+++ ninja_skills.rb
@@ -0,0 +1,14 @@
+# Challenge 1: Booleans 
+
+# 1. Create a variable a and set it equal to true
+a = true
+# 2. Create a variable b and set it equal to false
+b = false
+# 3.  Write a statement using a and b that evaluates to false
+    a === b 
+#       puts the result to the terminal
+# 4. Write a statement using a and b that evaluates to true
+    a != b 
+
+
+#       puts the result to the terminal
