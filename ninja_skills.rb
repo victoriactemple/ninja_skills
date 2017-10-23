101f617cdb559dbee7ee019a480c646f78e9fe8c
victoriactemple <victoriactemple@gmail.com>
2017-10-23 19:42:23 -0400

    5. Input/Output skills

--- ninja_skills.rb
+++ ninja_skills.rb
@@ -52,21 +52,48 @@
 #  ------------------------------------------------------
 
 
-# Challenge 4: Strings
+# # Challenge 4: Strings
 
-# 1. Create a variable christine and set it equal to the string "Hi, I'm Christine"
-christine = "Hi, I'm Christine"
-# 2. Create a variable likesTo and set it equal to the String "long walks on the beach, kinda."
-likes_to = "long walks on the beach, kinda."
-# 3. Using string interpolation and the variables tims and likesTo puts the following string to the terminal: "Hi, I'm Christine and I like long walks on the beach, kinda."
+# # 1. Create a variable christine and set it equal to the string "Hi, I'm Christine"
+# christine = "Hi, I'm Christine"
+# # 2. Create a variable likesTo and set it equal to the String "long walks on the beach, kinda."
+# likes_to = "long walks on the beach, kinda."
+# # 3. Using string interpolation and the variables tims and likesTo puts the following string to the terminal: "Hi, I'm Christine and I like long walks on the beach, kinda."
 
-tims = christine + ' and I like ' + likes_to
-puts tims
-# 4. Create a variable g and set it equal to the string "2"
-g = "2"
-# 5. Create a variable gToNumber and set it equal to the Integer 2
-g_to_number = 2
-# 6. puts the value of g + gToNumber in the terminal. What happens? How can you fix this?
-puts g + g_to_number.to_s
-_to string
-# 🎯 Commit -m "4. String skills "
+# tims = christine + ' and I like ' + likes_to
+# puts tims
+# # 4. Create a variable g and set it equal to the string "2"
+# g = "2"
+# # 5. Create a variable gToNumber and set it equal to the Integer 2
+# g_to_number = 2
+# # 6. puts the value of g + gToNumber in the terminal. What happens? How can you fix this?
+# puts g + g_to_number.to_s
+# _to string
+# # 🎯 Commit -m "4. String skills "
+
+
+# Challenge 5: Input and Output
+
+# 1.Create a small program that asks the user to input an Integer and multiplies the number by two.
+
+puts "Pick a number: "
+user_input = gets.chomp.to_i
+    product = user_input * 2
+    puts product
+
+   
+
+# 2. Create a small program that does the following
+
+#   Asks the user for their name
+#   Asks the user what they like to do
+#   Prints out the user's name and what they like to do in a sentence.
+
+puts "What is your name? "
+user_name = gets.chomp
+puts "What do you like to do for fun?"
+    user_hobby = gets.chomp
+print " #{user_name} likes to #{user_hobby} for fun"
+
+
+# 🎯 Commit -m "5. Input/Output skills"
